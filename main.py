270abde8100a6df43a223b0b1735f107a2d6a2ed
Varghese P Mathew <33537762+varghesepm@users.noreply.github.com>
2018-05-22 00:34:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,16 @@ def main():
     menu_list = [
       inquirer.List('selected_option',
                     message="Select an Option( use UP and DOWN keys)",
-                    choices=['Add server', 'Quit'],
+                    choices=['Add Server', 'List Servers', 'Quit'],
                 ),
     ]
     answers = inquirer.prompt(menu_list)
     # print(answers["selected_option"])
-    if answers["selected_option"] is "Add server":
-        print("yes")
-        return add_servers()
-    elif answers["selected_option"] is "Quit":
+    if answers["selected_option"] == "Add Server":
+        add_servers()
+    elif answers["selected_option"] == "List Servers":
+        return 0
+    elif answers["selected_option"] == "Quit":
         return 0
     else:
         print ("Unknown option Selected")
@@ -35,27 +36,50 @@ def add_servers():
     print("", header.upper())
     print("",'+'*len(header))
     print("")
+
     while True:
         try:
-            host_name = input("Enter your Hostname :")
-        except ValueError:
-            print("Please enter a valid hostname")
+            host_name = str(input("Please enter your Host name : "))
+        except:
+            print("Invalid Host Name")
             continue
+        break
+    while True:
         try:
-            host_ip = int(input("Enter your Host IP :"))
-        except ValueError:
-            print("Please enter a valid hostIP")
+            host_ip = int(input("Please enter your Host IP : "))
+        except:
+            print("Invalid Host IP")
             continue
+        break
+    while True:
         try:
-            host_user = input("Host user name :")
-        except ValueError:
-            print("Please enter a valid User name")
+            host_user = str(input("Please enter your Host Username : "))
+        except:
+            print("Invalid Host Username")
             continue
+        break
+    while True:
         try:
-            host_passwd = input("Enter Host Password :")
-        except ValueError:
-            print("Please enter a valid hostPassword")
+            host_passwd = str(input("Please enter your Host Password : "))
+        except:
+            print("Invalid Host Password")
             continue
+        break
+    while True:
+        try:
+            host_pubkey = str(input("Please enter your Host PubKey : "))
+            # print("successfully addded")
+        except:
+            print("Invalid Host PubKey")
+            continue
+        break
+    if host_name && host_ip && host_user && host_passwd && host_pubkey:
+        fun_addToDB()
+    else:
+        print("Something went wrong")
+
+def fun_addToDB():
+
 
 if __name__ == '__main__':
     main()
